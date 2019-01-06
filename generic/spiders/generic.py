09b454030a8a2e51c516222057dbd03064c22640
wansong <betterofsong@gmail.com>
2019-01-06 15:51:33 +0800

    start based on scrapy example code

---
+++ generic/spiders/generic.py
@@ -0,0 +1,38 @@
+import scrapy
+
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
+
+class GenericSpider(scrapy.Spider):
+    name = "generic"
+    def start_requests(self):
+        url = self.url
+        yield scrapy.Request(url, callback=self.parse,
+                             errback=self.errback_httpbin)
+
+    def parse(self, response):
+        self.logger.info('Got successful response from {}'.format(response.url))
+        # do something useful here...
+
+    def errback_httpbin(self, failure):
+        # log all failures
+        self.logger.error(repr(failure))
+
+        # in case you want to do something special for some errors,
+        # you may need the failure's type:
+
+        if failure.check(HttpError):
+            # these exceptions come from HttpError spider middleware
+            # you can get the non-200 response
+            response = failure.value.response
+            self.logger.error('HttpError on %s', response.url)
+
+        elif failure.check(DNSLookupError):
+            # this is the original request
+            request = failure.request
+            self.logger.error('DNSLookupError on %s', request.url)
+
+        elif failure.check(TimeoutError, TCPTimedOutError):
+            request = failure.request
+            self.logger.error('TimeoutError on %s', request.url)
